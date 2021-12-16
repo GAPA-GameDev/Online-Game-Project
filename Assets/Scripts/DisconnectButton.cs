ca347b69bc6f12184c72daacd40da0ca9cf101f3
Pau Pedra Bonifacio <paupe200@gmail.com>
2021-12-16 12:55:18 +0100

    Now players can connect and disconnect

---
+++ Assets/Scripts/DisconnectButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisconnectButton : MonoBehaviour
+{
+
+    public string clientName;
+
+    Peer2PeerClient client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = GameObject.Find(clientName).GetComponent< Peer2PeerClient>();
+
+        gameObject.GetComponent<Button>().onClick.AddListener(OnClickDisconnect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClickDisconnect()
+    {
+        client.OnDisconnect();
+    }
+}
